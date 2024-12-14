9d6f56b6977df199c907f0aa5cf8a6eb937ba496
YongHyun-Yi <dldydgus1995@naver.com>
2024-12-15 00:10:28 +0900

    wrap 을 사용해서 코드 리팩터링

--- TimeStruct.gd
+++ TimeStruct.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name TimeStruct
 
+signal ValueUpdate
+
 var Hour: int = 0 : set = SetHour
 var Min: int = 0 : set = SetMin
 var Sec: int = 0 : set = SetSec
@@ -12,41 +14,32 @@ func _init(h = 0, m = 0, s = 0):
 
 func SetHour(value: int):
 	Hour = wrap(value, 0, 24)
-
-func normalize_10(value: int) -> int:
-	# 60을 넘어가면 10의 자리를 0으로 설정
-	if value >= 60:
-		return value % 10
-	# 0보다 작다면 10의 자리를 5로 설정
-	elif value < 0:
-		return 50 + abs(value % 10)
-	return value
-
-func normalize_1(value: int) -> int:
-	# 내림이 발생했다면 1의 자리를정 9로 설정
-	if Min > value:
-		return (value / 10) + 9
-	# 올림이 발생했다면 1의 자리를 0으로 설정
-	else:
-		return value / 10
-	return value
+	emit_signal("ValueUpdate")
 
 func SetMin(value: int):
+	var new_10
+	var new_1
+	
 	if abs(Min - value) >= 10:
-		# 10의 자리만 수정하게 보정한다
-		value = normalize_10(value)
+		new_10 = wrap(value / 10, 0, 6)
+		new_1 = Min % 10
 	else:
-		# 1의 자리만 수정하게 보정한다
-		if (Min / 10 != value / 10):
-			value = normalize_1(value)
-	Min = value
+		new_10 = Min / 10
+		new_1 = wrap(value % 10, 0, 10)
+	
+	Min = (new_10 * 10) + new_1
+	emit_signal("ValueUpdate")
 
 func SetSec(value: int):
+	var new_10
+	var new_1
+	
 	if abs(Sec - value) >= 10:
-		# 10의 자리만 수정하게 보정한다
-		value = normalize_10(value)
+		new_10 = wrap(value / 10, 0, 6)
+		new_1 = Sec % 10
 	else:
-		# 1의 자리만 수정하게 보정한다
-		if (Sec / 10 != value / 10):
-			value = normalize_1(value)
-	Sec = value
+		new_10 = Sec / 10
+		new_1 = wrap(value % 10, 0, 10)
+	
+	Sec = (new_10 * 10) + new_1
+	emit_signal("ValueUpdate")
