9380e5da442654a2881b8a7667947fa2e443628e
YongHyun-Yi <dldydgus1995@naver.com>
2024-12-14 23:38:36 +0900

    시, 분, 초 setter 구현 및 값보정 하드코딩

--- TimeStruct.gd
+++ TimeStruct.gd
@@ -11,24 +11,42 @@ func _init(h = 0, m = 0, s = 0):
 	Sec = s
 
 func SetHour(value: int):
-	Hour = clamp(value, 0, 24)
+	Hour = wrap(value, 0, 24)
+
+func normalize_10(value: int) -> int:
+	# 60을 넘어가면 10의 자리를 0으로 설정
+	if value >= 60:
+		return value % 10
+	# 0보다 작다면 10의 자리를 5로 설정
+	elif value < 0:
+		return 50 + abs(value % 10)
+	return value
+
+func normalize_1(value: int) -> int:
+	# 내림이 발생했다면 1의 자리를정 9로 설정
+	if Min > value:
+		return (value / 10) + 9
+	# 올림이 발생했다면 1의 자리를 0으로 설정
+	else:
+		return value / 10
+	return value
 
 func SetMin(value: int):
-	if value >= 0:
-		if value >= 60:
-			Min = value % 60
-		else:
-			Min = value
+	if abs(Min - value) >= 10:
+		# 10의 자리만 수정하게 보정한다
+		value = normalize_10(value)
 	else:
-		# (-)버튼을 까먹고 있었다...
-		# 이때를 상정한 보정은 여기서
-		Min = 0
+		# 1의 자리만 수정하게 보정한다
+		if (Min / 10 != value / 10):
+			value = normalize_1(value)
+	Min = value
 
 func SetSec(value: int):
-	if value >= 0:
-		if value >= 60:
-			Sec = value % 60
-		else:
-			Sec = value
+	if abs(Sec - value) >= 10:
+		# 10의 자리만 수정하게 보정한다
+		value = normalize_10(value)
 	else:
-		Sec = 0
+		# 1의 자리만 수정하게 보정한다
+		if (Sec / 10 != value / 10):
+			value = normalize_1(value)
+	Sec = value
