1dc629c8f2caf87c70e63c8cb7a353e71f4c2933
YongHyun-Yi <dldydgus1995@naver.com>
2024-12-15 13:29:25 +0900

    Focus, Rest enum 구현, enum에 따라 타이머값 갱신, 타이머 만료시 토글

--- pomo_timer.gd
+++ pomo_timer.gd
@@ -1,17 +1,49 @@
 extends Control
+enum {FOCUS, REST}
 
+@export var CurState = FOCUS
+@export var IsPaused = false
 
-# Called when the node enters the scene tree for the first time.
+# 일단 시작은 집중 시간부터
+# 미리 time setting, pomo timer 씬이 로드되어있는 채로시작한다면
+# 이 부분은 들어낸다...
 func _ready() -> void:
-	$Timer.wait_time = (GlobalScript.FocusTimeData.Hour * 3600) + (GlobalScript.FocusTimeData.Min * 60) + GlobalScript.FocusTimeData.Sec
+	$Timer.wait_time = GetWaitTimeFromTs(GlobalScript.FocusTimeData)
 	$Timer.start()
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# 현재 타이머의 남은 시간을 계속해서 표시 함
 func _process(delta: float) -> void:
 	var time_left = int($Timer.time_left)
 	$Time/Hour.text = str(time_left / 3600)
 	$Time/Min.text = str("%02d" % (time_left % 3600 / 60))
 	$Time/Sec.text = str("%02d" % (time_left % 3600 % 60))
 	pass
+
+# TimeStruct의 시간을 초 단위로 반환
+# 혹시 모를 다수의 TImeStruct를 대비해서 모듈화 한다
+func GetWaitTimeFromTs(ts: TimeStruct) -> int:
+	var ret: int = 0;
+	
+	ret += ts.Hour * 3600
+	ret += ts.Min * 60
+	ret += ts.Sec
+	
+	return ret
+
+# 현재 시간이 종료되면 CurStats를 토글한다
+# 이 부분을 토글 따로 렌더 따로 나눌 수 있을까...
+func TimerTimeout() -> void:
+	match CurState:
+		FOCUS:
+			CurState = REST
+			$Label.text = "휴식 시간"
+			$Timer.wait_time = GetWaitTimeFromTs(GlobalScript.RestTimeData)
+			
+		REST:
+			CurState = FOCUS
+			$Label.text = "집중 시간"
+			$Timer.wait_time = GetWaitTimeFromTs(GlobalScript.FocusTimeData)
+	
+	$Timer.start()
+	pass # Replace with function body.
